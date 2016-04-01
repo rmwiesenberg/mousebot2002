c7bfbfddeeb0c5c3c786a7ede26d0fe1d6b530f1
rmwiesenberg <rmwiesenberg@wpi.edu>
2016-04-01 19:34:38 -0400

    Yaaaas

--- final/src/final.cpp
+++ final/src/final.cpp
@@ -1,13 +1,59 @@
 #include <Arduino.h>
+#include <Servo.h>
 #include "final.h"
 
-#define MAX_FRW 180
-#define MAX_REV 0
+#define RIGHT_MOTOR_PIN 4
+#define LEFT_MOTOR_PIN 5
+#define TURRET_MOTOR_PIN 6
+#define TURRET_SERVO_PIN 7
 
-void setup(){
+#define MOTOR_MAX_FRW 180
+#define MOTOR_STOP 90
+#define MOTOR_MAX_REV 0
 
+Servo rightMotor, leftMotor, turretMotor, turretServo;
+
+enum globalState{
+  INIT, FIND_FLAME, TO_FLAME, EXTINGUISH, FIND_WALL, GO_HOME, STOP
+};
+
+enum wallState{
+  RIGHT_FOLLOW, LEFT_FOLLOW, CORNER, WALL_END
+};
+
+globalState gState = FIND_FLAME;
+wallState wState;
+
+void setup() {
+  rightMotor.attach(RIGHT_MOTOR_PIN, 1000, 2000);
+  leftMotor.attach(LEFT_MOTOR_PIN, 1000, 2000);
+  turretMotor.attach(TURRET_MOTOR_PIN, 1000, 2000);
+  turretServo.attach(TURRET_SERVO_PIN);
 }
 
-void loop(){
+void loop() {
+  switch (gState) {
+    case INIT:
 
+    break;
+
+    case FIND_FLAME:
+
+    break;
+
+    case TO_FLAME:
+    break;
+
+    case EXTINGUISH:
+    break;
+
+    case FIND_WALL:
+    break;
+
+    case GO_HOME:
+    break;
+
+    case STOP:
+    break;
+  }
 }
