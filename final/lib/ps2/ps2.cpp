d64c96aef09044fce59fccacb5f5b3387adc7860
rmwiesenberg <rmwiesenberg@wpi.edu>
2016-04-05 13:05:23 -0400

    Comment
    I made a comment for richard

--- final/lib/ps2/ps2.cpp
+++ final/lib/ps2/ps2.cpp
@@ -1,4 +1,5 @@
 #include "ps2.h"
+// look I did a thing
 
 PS2::PS2(int clk, int data){
   _ps2clk=clk;
@@ -19,7 +20,7 @@ void PS2::golo(int pin){
 
 void PS2::write(unsigned char data){
   unsigned char parity=1;
-  
+
   gohi(_ps2data);
   gohi(_ps2clk);
   delayMicroseconds(300);
@@ -28,58 +29,58 @@ void PS2::write(unsigned char data){
   golo(_ps2data);
   delayMicroseconds(10);
   gohi(_ps2clk);
-  
+
   while(digitalRead(_ps2clk)==HIGH);
-  
+
   for(int i=0; i<8; i++){
     if(data&0x01) gohi(_ps2data);
-    else golo(_ps2data); 
+    else golo(_ps2data);
     while(digitalRead(_ps2clk)==LOW);
     while(digitalRead(_ps2clk)==HIGH);
     parity^=(data&0x01);
     data=data>>1;
   }
-  
+
   if(parity) gohi(_ps2data);
   else golo(_ps2data);
 
   while(digitalRead(_ps2clk)==LOW);
   while(digitalRead(_ps2clk)==HIGH);
-  
+
   gohi(_ps2data);
   delayMicroseconds(50);
-  
+
   while(digitalRead(_ps2clk)==HIGH);
   while((digitalRead(_ps2clk)==LOW)||(digitalRead(_ps2data)==LOW));
-  
+
   golo(_ps2clk);
 }
 
 unsigned char PS2::read(void){
   unsigned char data=0, bit=1;
- 
+
   gohi(_ps2clk);
   gohi(_ps2data);
   delayMicroseconds(50);
   while(digitalRead(_ps2clk)==HIGH);
-  
+
   delayMicroseconds(5);
   while(digitalRead(_ps2clk)==LOW);
-  
+
   for(int i=0; i<8; i++){
     while(digitalRead(_ps2clk)==HIGH);
     if(digitalRead(_ps2data)==HIGH) data|=bit;
     while(digitalRead(_ps2clk)==LOW);
     bit=bit<<1;
   }
-  
+
   while(digitalRead(_ps2clk)==HIGH);
   while(digitalRead(_ps2clk)==LOW);
   while(digitalRead(_ps2clk)==HIGH);
   while(digitalRead(_ps2clk)==LOW);
-  
+
   golo(_ps2clk);
-  
+
   return data;
 }
 
